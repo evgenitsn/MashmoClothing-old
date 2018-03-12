7a1ded5a009a1ed43731ee7f19e69fe49c0d4dd2
Evgeni Nikolov <evgeni.tsn@gmail.com>
2018-03-13 01:18:41 +0200

    Dummy Index Page Content

--- pages/index.js
+++ pages/index.js
@@ -4,8 +4,28 @@ import { Row, Col } from 'antd'
 
 export default () => (
   <Layout>
-    <Slider/>
+    {/*<Slider/>*/}
     <div style={{maxWidth: 1280, margin: '0 auto'}}>
+      <Row>
+        <Col span={24}>
+          <h1 style={{marginTop: 20, textAlign: 'center'}}>FEATURED</h1>
+          <h1 style={{marginTop: 20, textAlign: 'center'}}>FEATURED</h1>
+          <h1 style={{marginTop: 20, textAlign: 'center'}}>FEATURED</h1>
+          <h1 style={{marginTop: 20, textAlign: 'center'}}>FEATURED</h1>
+          <h1 style={{marginTop: 20, textAlign: 'center'}}>FEATURED</h1>
+          <h1 style={{marginTop: 20, textAlign: 'center'}}>FEATURED</h1>
+          <h1 style={{marginTop: 20, textAlign: 'center'}}>FEATURED</h1>
+          <h1 style={{marginTop: 20, textAlign: 'center'}}>FEATURED</h1>
+          <h1 style={{marginTop: 20, textAlign: 'center'}}>FEATURED</h1>
+          <h1 style={{marginTop: 20, textAlign: 'center'}}>FEATURED</h1>
+          <h1 style={{marginTop: 20, textAlign: 'center'}}>FEATURED</h1>
+          <h1 style={{marginTop: 20, textAlign: 'center'}}>FEATURED</h1>
+          <h1 style={{marginTop: 20, textAlign: 'center'}}>FEATURED</h1>
+          <h1 style={{marginTop: 20, textAlign: 'center'}}>FEATURED</h1>
+          <h1 style={{marginTop: 20, textAlign: 'center'}}>FEATURED</h1>
+          <h1 style={{marginTop: 20, textAlign: 'center'}}>FEATURED</h1>
+        </Col>
+      </Row>
       <Row>
         <Col span={12}>col-12</Col>
         <Col span={12}>col-12</Col>

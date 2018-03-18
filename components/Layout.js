d2763881b36f11771dbdd98d8dc150dc638121cb
Evgeni Nikolov <evgeni.tsn@gmail.com>
2018-03-18 23:40:08 +0200

    Libs cleanup

--- components/Layout.js
+++ components/Layout.js
@@ -10,10 +10,8 @@ export default ({children, title = defaultTitle}) => (
       <title>{title}</title>
       <meta charSet='utf=8' />
       <meta name='viewport' content='initial-scale=1.0, width=device-width' />
-      <link rel='stylesheet' type="text/css" charset="UTF-8" href='../static/styles/antd.css' />
+      <link rel='stylesheet' type="text/css" charset="UTF-8" href='https://unpkg.com/antd@3/dist/antd.min.css' />
       <link rel='stylesheet' type="text/css" charset="UTF-8" href='../static/styles/antd-override.css' />
-      <link rel="stylesheet" type="text/css" charset="UTF-8" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css" />
-      <link rel="stylesheet" type="text/css" charset="UTF-8" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css" />
     </Head>
     <Header/>
     <div>

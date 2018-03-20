ac21b9ad8d74e534491e5189aef5ad3e3305d6e7
Evgeni Nikolov <evgeni.tsn@gmail.com>
2018-03-20 02:15:06 +0200

    Product Data Placeholders

--- pages/product.js
+++ pages/product.js
@@ -1,9 +1,32 @@
+import { Row, Col } from 'antd'
 import Layout from '../components/Layout'
 
 const Post = (props) => (
   <Layout>
-    <h1>{props.product.name}</h1>
-    <h1>{props.product.price}</h1>
+    <div style={styles.container}>
+      <h1>{props.product.name}</h1>
+      <h1>{props.product.price}</h1>
+      <Row>
+        <Col span={8} style={{border: '1px solid green', width: '33%', padding: '5%'}}>
+          <img src={'../static/cap.jpg'} width='100%'/>
+        </Col>
+        <Col span={16} style={{border: '1px solid blue', display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column'}}>
+          <h1 style={styles.title}>DETAILS</h1>
+          <h2>Title / Name</h2>
+          <h2>Price</h2>
+          <h3>In Stock / Out of Stock</h3>
+          <h4>Size</h4>
+          <select name="Select Size">
+            <option>XS</option>
+            <option>S</option>
+            <option>M</option>
+          </select>
+          <h4>Quantity</h4>
+          <input type="number" max="5" min="1"/>
+          <button>Add to Cart</button>
+        </Col>
+      </Row>
+    </div>
   </Layout>
 )
 
@@ -19,3 +42,18 @@ Post.getInitialProps = async function (context) {
 }
 
 export default Post
+
+const styles = {
+  container: {
+    maxWidth: 1280, 
+    margin: '0 auto',
+    marginBottom: '5%'
+  },
+  title: {
+    marginTop: '2%',
+    marginBottom: '5%',
+    textAlign: 'center',
+    fontSize: '5em',
+    fontFamily: 'Regensburg'
+  },
+}
